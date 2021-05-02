7fb315050902187a798496a166f523b0f124d096
J Fav <jfavorito@outlook.com>
2021-05-02 16:38:59 -0700

    added environment variables

---
+++ src/aws-exports.js
@@ -0,0 +1,11 @@
+
+const awsmobile = {
+    "aws_project_region": process.env.PROJECT_REGION,
+    "aws_appsync_graphqlEndpoint": process.env.GRAPHQL_ENDPOINT,
+    "aws_appsync_region": process.env.APPSYNC_REGION,
+    "aws_appsync_authenticationType": process.env.APPSYNC_AUTH_TYPE,
+    "aws_appsync_apiKey": process.env.APPSYNC_API_KEY
+};
+
+
+export default awsmobile;
