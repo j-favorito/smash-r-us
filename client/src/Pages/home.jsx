3b58869c2d910cb3c55cec79561dee6ab52b7e4b
J Fav <jfavorito@outlook.com>
2021-05-02 15:02:46 -0700

    added working database

--- client/src/Pages/home.jsx
+++ client/src/Pages/home.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import Heading from "../Components/heading"
+import Amplify from '@aws-amplify/core';
+import awsExports from '../aws-exports';
+import { DataStore } from '@aws-amplify/datastore';
+import { Users } from '../models/index.js';
 
-function Home(){
+function Home() {
 
-    return(
+    const saveUser = async () => {
+        try {
+            Amplify.configure(awsExports);
+            await DataStore.save(
+                new Users({
+                    "username": "Lorem ipsum dolor sit amet",
+                    "portrait": "Lorem ipsum dolor sit amet",
+                    "wins": 5,
+                    "losses": 10
+                })
+            );
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+    return (
         <div>
-        <Heading />
+            <Heading />
+            <button onClick={saveUser} >Click ME!</button>
         </div>
     )
 
---
+++ client/src/models/schema.js
@@ -0,0 +1,71 @@
+export const schema = {
+    "models": {
+        "Users": {
+            "name": "Users",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "username": {
+                    "name": "username",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "portrait": {
+                    "name": "portrait",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "wins": {
+                    "name": "wins",
+                    "isArray": false,
+                    "type": "Int",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "losses": {
+                    "name": "losses",
+                    "isArray": false,
+                    "type": "Int",
+                    "isRequired": false,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "Users",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "allow": "public",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete",
+                                    "read"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "eca9646f075c4f979705d9e5e3ca07d2"
+};
